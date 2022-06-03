e76eb6383314ee0cff9a3bad6d1a7510e18ab093
cordeiro100 <mosscordeiro@hotmail.com>
2022-06-03 18:58:12 -0300

    ctd3

---
+++ JS/arrays.js
@@ -0,0 +1,20 @@
+let numbers = []
+
+numbers.push(2)
+
+console.log(numbers.push(2))
+
+
+numbers.push(3)
+
+console.log(numbers)
+
+
+numbers.pop()
+console.log(numbers)
+
+numbers.push(5)
+console.log(numbers)
+
+numbers.shift()
+console.log(numbers)
---
+++ JS/objects.js
@@ -0,0 +1,13 @@
+let car = {
+    brand: "Chevrolet",
+    model: "Corsa",
+    price: 32000
+}
+
+console.log(car)
+
+car.color = "red";
+
+console.log(car)
+
+
